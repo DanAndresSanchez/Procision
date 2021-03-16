3b3e96343b8114208199a5244dba485efd46c5be
Dan Sanchez <32966376+DanAndresSanchez@users.noreply.github.com>
2021-03-15 20:43:24 -0400

    Initial Commit

---
+++ DataClense.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace Procision
+{
+    class DataClense
+    {
+        int lastUsedRow, lastUsedCol;
+        _Excel.Worksheet final_report;
+
+        public void centerAlign()
+        {
+            var rng = final_report.get_Range("A1:R300", Type.Missing);
+            rng.Cells.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+            rng.Cells.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+            rng.Cells.Columns.AutoFit();
+            rng.Cells.Rows.AutoFit();
+        }
+
+        public void clearSheet()
+        {
+            getSheetInfo();
+            var rng = final_report.get_Range("A2:Z300", Type.Missing);
+            rng.Cells.Clear();
+            Console.WriteLine("Sheet Cleared");
+        }
+
+        //Working on this in the reportFromDB() method, feel free to use this instead if its easier
+        public void createBorder()
+        {
+            string previous = "";
+            string current = "";
+
+            for (int row = 2; row < lastUsedRow; row++)
+            {
+                current = "";
+                current += final_report.Cells[row, 2].Value() + "," + final_report.Cells[row, 3].Value() + "," + final_report.Cells[row, 4].Value();
+                if (row != 2)
+                {
+                    if (!(previous.Equals(current)))
+                    {
+                        var rng = "A" + row + ":R" + lastUsedRow;
+                        final_report.Cells.Range[rng].Borders[_Excel.XlBordersIndex.xlEdgeTop].Weight = 3d;
+                    }
+                }
+
+                previous = current;
+            }
+        }
+
+        //Initializes final report sheet
+        public void getSheetInfo()
+        {
+            final_report = Globals.ThisAddIn.Application.ActiveSheet;
+            _Excel.Range last = final_report.Cells.SpecialCells(_Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+            lastUsedRow = last.Row;
+            lastUsedCol = last.Column;
+        }
+    }
+}
---
+++ Ribbon1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Procision
+{
+    public partial class Ribbon1
+    {
+        DataClense dc = new DataClense();
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            dc.clearSheet();
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            dc.centerAlign();
+        }
+    }
+}
